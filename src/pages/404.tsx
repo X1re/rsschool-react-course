81e611474c7596ffb9a990fe4f9ccaa0c8dbe33c
X1re <infin753@gmail.com>
2023-03-31 17:22:36 +0300

    refactor: rewrite 404 and AboutUs to functional component

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,10 @@
-import { Component } from 'react';
-
-class NotFound extends Component {
-  render() {
-    return (
-      <div>
-        <h1>404 Page Not Found</h1>
-        <p>The requested URL was not found on this server.</p>
-      </div>
-    );
-  }
-}
+const NotFound = () => {
+  return (
+    <div>
+      <h1>404 Page Not Found</h1>
+      <p>The requested URL was not found on this server.</p>
+    </div>
+  );
+};
 
 export default NotFound;
--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,13 +1,9 @@
-import { Component } from 'react';
-
-class AboutUs extends Component {
-  render() {
-    return (
-      <div>
-        <h1>About Us</h1>
-        <p>Here we publish cute animals</p>
-      </div>
-    );
-  }
-}
+const AboutUs = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Here we publish cute animals</p>
+    </div>
+  );
+};
 export default AboutUs;
